a9358581d5389b53cefb6994e97a5c191a61ed56
Maria Aboulaala <mariaaboulaala28@gmail.com>
2023-05-08 19:09:11 +0100

    last last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import yfinance as yf
+#import yfinance as yf
 import numpy as np
 from datetime import datetime, timedelta
 from scipy.stats import norm
